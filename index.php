57699eb0bb119d133190f16662ec27ea2b8cc969
redoyRKS <rshaha201355@bscse.uiu.ac.bd>
2022-08-06 23:16:09 +0600

    tata

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+  #comparisond booleans(true or false)
+  //echo true; "1"
+  //echo false; "emty"
+  
+  //number
+  //echo 5<10;
+  //echo 5>10;
+  //echo 5==10;
+  //echo 10 == 10;  //"1"
+ // echo 5!=10;    // "1"
+ //echo 5 <= 5;   //"1"
+ //echo 5 >= 5;  //"1"
+
+
+ //string
+
+echo 'redoy' < 'oishi';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+  <title> PHP Tutorial </title>
+</head>
+<body>
+    
+</body>
+</html>
